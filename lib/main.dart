cdb19f8d15980284999aec73e1b6a6b1eeecde48
Strait21 <jared.strait21@gmail.com>
2021-11-20 23:23:38 -0500

    Added View player list and delte player,noconfir

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
+//import 'dart:html';
 
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -164,12 +167,7 @@ class _PlayerHomeState extends State<PlayerHome> {
 
   void viewPlayers() {
     Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => PlayerList(
-                  title: 'Player List',
-                  user: widget.user,
-                )));
+        context, MaterialPageRoute(builder: (context) => UserInformation()));
   }
 
 // 888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
@@ -187,19 +185,29 @@ class _PlayerHomeState extends State<PlayerHome> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: ElevatedButton(
-                      onPressed: _setSignIn,
-                      child: const Text("Sign in"),
-                      // Need to change the size of the buttons and add the sending of the data to firebase for both of them
-                    )),
-                Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: ElevatedButton(
-                      onPressed: _setSignOut,
-                      child: const Text("Sign out"),
-                    )),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: ElevatedButton(
+                          onPressed: _setSignIn,
+                          child: const Text("Sign in"),
+                          // Need to change the size of the buttons and add the sending of the data to firebase for both of them
+                        )),
+                    Text(_signInTime),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: ElevatedButton(
+                          onPressed: _setSignOut,
+                          child: const Text("Sign out"),
+                        )),
+                    Text(_signOutTime),
+                  ],
+                ),
               ],
             ),
             Padding(
@@ -371,43 +379,111 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-class PlayerList extends StatefulWidget {
-  const PlayerList({Key? key, required this.title, required this.user})
-      : super(key: key);
-  final String title;
-  final String user;
-
+class UserInformation extends StatefulWidget {
   @override
-  State<PlayerList> createState() => _PlayerList();
+  _UserInformationState createState() => _UserInformationState();
 }
 
-class _PlayerList extends State<PlayerList> {
-  @override
-  void initState() {
-    super.initState();
-    Firebase.initializeApp();
+class _UserInformationState extends State<UserInformation> {
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('Users').snapshots();
+
+  Widget _buildList(BuildContext context, DocumentSnapshot document) {
+    if (document.data()['date'] == '') return const SizedBox(height: 10.0);
+    return ListTile(
+      title: Row(
+        children: [
+          Column(
+            children: [
+              Text(document.data()['Name']),
+              Text(
+                  ' ${document.data()['Sign in time']} : ${document.data()['Sign out time']} : ${document.data()['date']}'),
+            ],
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 50.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        FirebaseFirestore.instance
+                            .collection("Users")
+                            .doc(document.id)
+                            .delete();
+                      },
+                      child: Text("Delete"))
+                ],
+              )),
+        ],
+      ),
+    );
   }
 
-// 888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-//                                Build
   @override
   Widget build(BuildContext context) {
-    CollectionReference users = FirebaseFirestore.instance.collection('Users');
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('PlayerList'),
       ),
-      // body: StreamBuilder(
-      //     stream: FirebaseFirestore.instance.collection("Users").snapshots(),
-      //     builder:(context, snapshot) {
-      //       if (!snapshot.hasData) return const Text('Loading...');
-      //       return ListView.builder(
-      //         itemExtent: 88.0,
-      //         itemBuilder: (context, index) =>
-      //           );
-      //     } ,
-      // ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('Users').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text("Loading...");
+          return ListView.builder(
+            itemCount: snapshot.data!.size,
+            itemBuilder: (context, index) =>
+                _buildList(context, snapshot.data!.docs[index]),
+          );
+        },
+      ),
     );
   }
 }
+
+
+// class PlayerList extends StatefulWidget {
+//   const PlayerList({Key? key, required this.title, required this.user})
+//       : super(key: key);
+//   final String title;
+//   final String user;
+
+//   @override
+//   State<PlayerList> createState() => _PlayerList();
+// }
+
+// class _PlayerList extends State<PlayerList> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     Firebase.initializeApp();
+//   }
+
+// // 888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+// //                                Build
+//   @override
+//   Widget build(BuildContext context) {
+//     CollectionReference users = FirebaseFirestore.instance.collection('Users');
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: StreamBuilder(
+//           stream: FirebaseFirestore.instance.collection('Users').snapshots(),
+//           builder: (context, snapshot) {
+//             if (!snapshot.hasData) return const Text('Loading...');
+//             return ListView(
+//               children: snapshot.data!.docs.map((DocumentSnapshot document) {
+//                 Map<String, dynamic> data =
+//                     document.data()! as Map<String, dynamic>;
+//                 return ListTile(
+//                   title: Text(data['name']),
+//                   subtitle: Text(data['date']),
+//                 );
+//               }).toList(),
+//             );
+//           }),
+//     );
+//   }
+// }
